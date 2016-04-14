67ad80682e5730cb45bb7d1d5d32cfc21bcc1b01
Tatsiana Chypsanava <camden_chip@hotmail.com>
2016-04-15 10:26:05 +1200

    adding radio form validation

---
+++ js/patterns.js
@@ -0,0 +1,2 @@
+var usernamePattern = new RegExp("^[a-zA-Z0-9.\-_]{3,20}$");
+	var namePattern = new RegExp("^[a-zA-Z' .-]{1,30}$");
--- js/validation.js
+++ js/validation.js
@@ -2,15 +2,17 @@
 var registrationForm = document.querySelector('#registration');
 // wait forthe user to submit the form
 registrationForm.onsubmit = function(event) {
+	var totalErrors = 0;
 	
 	//patterns used for validation
-	var usernamePattern = new RegExp("^[a-zA-Z0-9.\-_]{3,20}$");
-	var namePattern = new RegExp("^[a-zA-Z' .-]{1,30}$");
+	
 	//get reference to the imput fields
-	var usernameInput =document.querySelector("#username");
+	var usernameInput = document.querySelector("#username");
 	var usernameMessage = document.querySelector("#username-message");
-	var nameInput = document.querySelector("#fullname");
+	var nameInput = document.querySelector("#full-name");
 	var nameMessage = document.querySelector("#fullname-message");
+	var campusMessage = document.querySelector('#campus-message');
+	var campusOptions = document.querySelectorAll('[name=campus]');
 
 	//check the user name
 	if(usernamePattern.test(usernameInput.value)) {
@@ -21,19 +23,35 @@ registrationForm.onsubmit = function(event) {
 	} else {
 		
 		usernameMessage.innerHTML = "Username is invalid";
+		totalErrors++;
 	}
 
-	if(namePattern.test(usernameInput.value)) {
+	if(namePattern.test(nameInput.value)) {
 		//name is valid
 		
 		nameMessage.innerHTML = "";
-	var nameInput = document.querySelector("#fullname");
 	} else {
-		
-		nameMessage.innerHTML = "Username is invalid";
+		nameMessage.innerHTML = "name is invalid";
+		totalErrors++;
 	}
-
+	//loop over all campus options
+	var campusIsSelected = false;
+for (var i=0; i < campusOptions.length; i++) {
+	//check if this campus has been selected
+	if( campusOptions[i].checked ) {
+		campusIsSelected = true;
+	}
+}
+//If campusIsSelected is still false
+if( campusIsSelected == false) {
+campusMessage.innerHTML = 'Please select a campus';
+totalErrors++;
+} else {
+	campusMessage.innerHTML ='';
+}
+//if the total errors greater than 0
+if( totalErrors > 0) {
 //stop th form from submition
- event.preventDefault();
+ event.preventDefault();}
 
 }
